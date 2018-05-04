334556e248f2bc7f647adaf78f35f2e50449de3f
KCK <chrud66@gmail.com>
2018-05-04 20:08:57 +0900

    study

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,7 +14,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        //기본 스트링 길이 지정
+        Schema::defaultStringLength(191);
     }
 
     /**
