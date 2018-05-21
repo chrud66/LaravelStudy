3ae6607ac6f612f8b8cea2ad0dfc044be899f776
KCK <chrud66@gmail.com>
2018-05-21 18:41:39 +0900

    study

--- app/Http/Controllers/OrmController.php
+++ app/Http/Controllers/OrmController.php
@@ -56,7 +56,7 @@ class OrmController extends Controller
                 ->skip(5)
                 ->take(3)
                 ->first();
-        */
+
 
         $tasks = Task::whereNotIn('id', [1, 5, 7])
                 ->get();
@@ -77,12 +77,111 @@ class OrmController extends Controller
                 })
                 ->get();
 
-        /*foreach($tasks as $task) {
+        foreach($tasks as $task) {
             echo "ID : " . $task->id . ", Name : " . $task->name . '<br/>';
-        }*/
+        }
+
+        //return response()->json($tasks, 200, [], JSON_PRETTY_PRINT);
+        */
+
+        /*
+        //insert
+        $task = new Task;
+
+        $task->name = '예제 작성';
+        $task->project_id = 1;
+        $task->description = 'insert 예제 작성';
+
+        $ret = $task->save();
+
+        return response()->json(['result' => $ret, 'task' => $task], 200, [], JSON_PRETTY_PRINT);
+        */
+
+        //update
+        /*
+        $task = Task::find(121);
+
+        $task->name = '갱신2 - 예제 작성';
+        $task->project_id = 1;
+        $task->description = '갱신2 - insert 예제 작성';
+
+        $ret = $task->save();
+        return response()->json(['result' => $ret, 'task' => $task], 200, [], JSON_PRETTY_PRINT);
+        */
+
+        /*
+        //update 2
+        $param = [
+            'name' => '갱신5 - 예제 작성',
+            'project_id' => 1,
+            'description' => '갱신5 - insert 예제 작성'
+        ];
+
+        $before = Task::find(121);
+        $ret = Task::find(121)->update($param);
+
+        $after = Task::find(121);
+        return response()->json(['result' => $ret, 'before' => $before, 'after' => $after], 200, [], JSON_PRETTY_PRINT);
+        */
+
+        /*
+        //insert2
+        $param = [
+            'name' => '예제 작성',
+            'project_id' => 1,
+            'description' => 'insert mass 예제 작성'
+        ];
+
+        $task = Task::create($param);
+
+        return response()->json($task, 200, [], JSON_PRETTY_PRINT);
+        */
+
+        /*
+        //delete
+        $task = Task::findOrFail(123);
+
+        $ret = $task->delete();
+
+        return response()->json(['ret' => $ret], 200, [], JSON_PRETTY_PRINT);
+        */
+
+        /*
+        //delete2
+        //$ret = Task::destroy(120,121,122,123,124);
+        $ret = Task::destroy([120,121,122,123,124]);
+        return response()->json(['ret' => $ret], 200, [], JSON_PRETTY_PRINT);
+        */
+
+        /*
+        //delete3
+        //소프트 델리트 적용으로 삭제 날짜 업데이트
+        $ret = Task::where('id', '>', 100)->where('id', '<', 110)->delete();
+
+        return response()->json(['ret' => $ret], 200, [], JSON_PRETTY_PRINT);
+        */
+
+        /*
+        //소프트 삭제된 레코드 가져오기
+        $tasks = Task::withTrashed()->where('id', '>', 99)->get(); //삭제 & 정상데이터
+        $tasks = Task::onlyTrashed()->where('id', '>', 99)->get(); //삭제된 데이터만
+
+        return response()->json($tasks, 200, [], JSON_PRETTY_PRINT);
+        */
+
+        /*
+        //소프트 삭제된 데이터 복구
+        $ret = Task::withTrashed()->find(108)->restore();
+
+        return response()->json(['ret' => $ret], 200, [], JSON_PRETTY_PRINT);
+        */
+
+        //소프트 삭제된 데이터 영구삭제
+        $ret = Task::withTrashed()->find(108)->forceDelete();
+
+        return response()->json(['ret' => $ret], 200, [], JSON_PRETTY_PRINT);
 
         //return $tasks;
-        return response()->json($tasks, 200, [], JSON_PRETTY_PRINT);
     }
 
     private function findWhereIn() {
--- app/Task.php
+++ app/Task.php
@@ -3,7 +3,18 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Task extends Model
 {
+    use SoftDeletes;
+
+    protected $dates = ['deleted_at'];
+
+    //화이트 리스트 방식
+    //protected $fillable = ['name', 'project_id', 'description'];
+    protected $fillable = ['name', 'project_id'];
+
+    //블랙 리스트 방식
+    //protected $guarded = ['description'];
 }
