8808fd0dd7f8b7e707d876bea9e837f1736443f3
chrud66 <chrud66@gmail.com>
2018-08-13 23:10:05 +0900

    envoy

--- envoy.blade.php
+++ envoy.blade.php
@@ -15,12 +15,12 @@
 #--------------------------------------------------------------------------
 ##
 
-@servers(['vm' => 'deployer@envoy.vm'])
+@servers(['vm' => 'deployer@kcklaravel'])
 
 
 @setup
   $username = 'deployer';                       // username at the server
-  $remote = 'https://github.com/appkr/envoy.git';
+  $remote = 'https://github.com/chrud66/laravel.git';
   // $remote = 'git@github.com:appkr/envoy.git';   // github repository to clone
   $base_dir = "/home/{$username}/www";          // document that holds projects
   $project_root = "{$base_dir}/envoy.vm";       // project root
@@ -42,8 +42,13 @@
     "{$shared_dir}/storage" => "{$release_dir}/{$distname}/storage",
     "{$shared_dir}/cache" => "{$release_dir}/{$distname}/bootstrap/cache",
   ];
+
+  $branch = "master";
 @endsetup
 
+@task('foo', ['on' => 'vm'])
+    ls -la
+@endtask
 
 @task('hello', ['on' => ['vm']])
   HOSTNAME=$(hostname);
@@ -57,43 +62,12 @@
     [ ! -d {{ $dir }} ] && mkdir -p {{ $dir }};
   @endforeach
 
-  {{--Download book keeping officer--}}
-  if [ ! -f {{ $base_dir }}/officer.php ]; then
-    wget https://raw.githubusercontent.com/appkr/envoy/master/scripts/officer.php -O {{ $base_dir }}/officer.php;
-  fi;
-
-  {{--Clone code from git--}}
-  cd {{ $release_dir }} && git clone -b master {{ $remote }} {{ $distname }};
-
-  [ ! -f {{ $shared_dir }}/.env ] && \
-    [ -f {{ $release_dir }}/{{ $distname }}/.env.example ] && \
-    cp {{ $release_dir }}/{{ $distname }}/.env.example {{ $shared_dir }}/.env;
-  [ ! -d {{ $shared_dir }}/storage ] && \
-    [ -d {{ $release_dir }}/{{ $distname }}/storage ] && \
-    cp -R {{ $release_dir }}/{{ $distname }}/storage {{ $shared_dir }};
-  [ ! -d {{ $shared_dir }}/cache ] && \
-    [ -d {{ $release_dir }}/{{ $distname }}/bootstrap/cache ] && \
-    cp -R {{ $release_dir }}/{{ $distname }}/bootstrap/cache {{ $shared_dir }};
-
-  {{--Symlink shared directory to current release.--}}
-  {{--e.g. storage, .env, user uploaded file storage, ...--}}
-  @foreach($shared_item as $global => $local)
-    [ -f {{ $local }} ] && rm {{ $local }};
-    [ -d {{ $local }} ] && rm -rf {{ $local }};
-    [ -f {{ $global }} ] && ln -nfs {{ $global }} {{ $local }};
-    [ -d {{ $global }} ] && ln -nfs {{ $global }} {{ $local }};
-  @endforeach
+  git pull origin {{ $branch }}
 
   {{--Run composer install--}}
   cd {{ $release_dir }}/{{ $distname }} && \
     [ -f ./composer.json ] && \
     composer install --prefer-dist --no-scripts --no-dev;
-
-  {{--Any additional command here--}}
-  {{--e.g. php artisan clear-compiled;--}}
-
-  {{--Symlink current release to service directory.--}}
-  ln -nfs {{ $release_dir }}/{{ $distname }} {{ $project_root }};
 
   {{--Set permission and change owner--}}
   [ -d {{ $shared_dir }}/storage ] && \
@@ -102,78 +76,7 @@
     chmod -R 775 {{ $shared_dir }}/cache;
   chgrp -h -R www-data {{ $release_dir }}/{{ $distname }};
 
-  {{--Book keeping--}}
-  php {{ $base_dir }}/officer.php deploy {{ $release_dir }}/{{ $distname }};
-
   {{--Restart web server.--}}
   sudo service nginx restart;
   sudo service php7.0-fpm restart;
-@endtask
-
-
-@task('prune', ['on' => 'vm'])
-  if [ ! -f {{ $base_dir }}/officer.php ]; then
-    echo '"officer.php" script not found.';
-    echo '\$ envoy run hire_officer';
-    exit 1;
-  fi;
-
-  @if (isset($keep) and $keep > 0)
-    php {{ $base_dir }}/officer.php prune {{ $keep }};
-  @else
-    echo 'Must provide --keep=n, where n is a number.';
-  @endif
-@endtask
-
-
-@task('hire_officer', ['on' => 'vm'])
-  {{--Download "officer.php" to the server--}}
-  wget https://raw.githubusercontent.com/appkr/envoy/master/scripts/officer.php -O {{ $base_dir }}/officer.php;
-  echo '"officer.php" is ready! Ready to roll master!';
-@endtask
-
-
-@task('list', ['on' => 'vm'])
-  {{--Show the list of release--}}
-  if [ ! -f {{ $base_dir }}/officer.php ]; then
-    echo '"officer.php" script not found.';
-    echo '\$ envoy run hire_officer';
-    exit 1;
-  fi;
-
-  php {{ $base_dir }}/officer.php list;
-@endtask
-
-
-@task('checkout', ['on' => 'vm'])
-  {{--checkout to the given release path--}}
-  if [ ! -f {{ $base_dir }}/officer.php ]; then
-    echo '"officer.php" script not found.';
-    echo '\$ envoy run hire_officer';
-    exit 1;
-  fi;
-
-  @if (isset($release))
-    cd {{ $release }};
-
-    {{--Symlink shared directory to the given release.--}}
-    @foreach($shared_item as $global => $local)
-      [ -f {{ $local }} ] && rm {{ $local }};
-      [ -d {{ $local }} ] && rm -rf {{ $local }};
-      ln -nfs {{ $global }} {{ $local }};
-    @endforeach
-
-    {{--Symlink the given release to service directory.--}}
-    ln -nfs {{ $release }} {{ $project_root }};
-
-    {{--Book keeping--}}
-    php {{ $base_dir }}/officer.php checkout {{ $release }};
-    chgrp -h -R www-data {{ $release_dir }}/{{ $distname }};
-
-    {{--Restart web server.--}}
-    sudo service nginx restart;
-    sudo service php7.0-fpm restart;
-  @else
-    echo 'Must provide --release=/full/path/to/release.';
-  @endif
 @endtask
